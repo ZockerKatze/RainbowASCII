64cbdfc924adc0be721aa201f30186f9bc3c72a2
Rattatwingo <89251490+ZockerKatze@users.noreply.github.com>
2024-12-18 19:34:43 +0100

    pretty shitty use of directories

--- original/function/bye.py
+++
@@ -1,5 +0,0 @@
-import asciiarg as aa
-
-aa.RAP("Bye Bye! :3")
-
-input("ENTER TO EXIT: ")
--- original/runfile.sh
+++
@@ -1,2 +0,0 @@
-cd /home/rattatwingo/Desktop/python/hfi-main/examples/colorbye/
-python3 /home/rattatwingo/Desktop/python/hfi-main/examples/colorbye/
